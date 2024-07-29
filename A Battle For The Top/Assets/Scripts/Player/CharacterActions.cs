86847772f0643318c499db924c138e9bb2df3b01
kalebtaylor3 <taylorkaleb16@gmail.com>
2024-07-29 00:27:49 -0400

    ProjectSetup

---
+++ A Battle For The Top/Assets/Scripts/Player/CharacterActions.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace BFTT.Abilities
+{
+    public class CharacterActions
+    {
+        public Vector2 move = Vector2.zero;
+
+        public bool jump = false;
+        public bool walk = false;
+        public bool roll = false;
+        public bool crouch = false;
+        public bool drop = false;
+        public bool crawl = false;
+        public bool interact = false;
+
+        public bool zoom = false;
+    }
+}
